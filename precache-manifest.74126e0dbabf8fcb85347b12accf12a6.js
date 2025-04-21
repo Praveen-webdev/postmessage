c54a5e6b7d4f650c812f4311add41fe6cce1b001
praveen.s <praveen.selvakumar@zohocorp.com>
2025-04-22 00:16:12 +0530

    Updates

---
+++ precache-manifest.74126e0dbabf8fcb85347b12accf12a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7cdbde6c5ca14064701d7555ffc7f51",
+    "url": "/postmessage-demo/index.html"
+  },
+  {
+    "revision": "725774281374cdcac409",
+    "url": "/postmessage-demo/static/css/main.132cf680.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/postmessage-demo/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/postmessage-demo/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "725774281374cdcac409",
+    "url": "/postmessage-demo/static/js/main.13434a6f.chunk.js"
+  },
+  {
+    "revision": "92492033da775934512e",
+    "url": "/postmessage-demo/static/js/runtime~main.438d678c.js"
+  }
+]);
